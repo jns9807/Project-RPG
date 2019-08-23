97fba4d506103982172d39bac07fedd4c1732b21
jns9807 <jns9807@gmail.com>
2019-08-22 23:35:52 -0500

    Added Enemy Class

---
+++ Project RPG/Enemies/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Enemies
+
+{
+    public class Enemy
+    {
+        private int Level { get; }
+        private int CurrentExperience;
+        private int HitPoints;
+        private int Strength;
+        private int Intelligence;
+        private int ExperienceGain;
+        private int Wisdom;
+
+        public Enemy() // Constructor
+        {
+            this.Level = 1;
+            this.CurrentExperience = 0;
+            this.HitPoints = 25;
+            this.Strength = 5;
+            this.Intelligence = 10;
+            this.ExperienceGain = 5;
+            this.Wisdom = 5;
+
+        }
+
+        public void GetHitPoints()
+        {
+            Console.WriteLine("Hitpoints: {0}", this.HitPoints);
+        }
+
+
+
+    }
+
+
+
+}
+
