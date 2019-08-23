2a0a186a480d9a012c3941494c9ad7e1c9df1e2f
JoshuaKSmith <joshuaksmith17@gmail.com>
2019-08-23 18:58:57 -0500

    Adding some looping functionality to eventually handle the game state.

--- Project RPG/Program.cs
+++ Project RPG/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Characters;
+using Enemies;
 
 namespace Program
 {
@@ -8,7 +9,16 @@ namespace Program
         static void Main(string[] args)
         {
             Character char1 = new Character();
+            String UserInput;
             char1.PrintCharacter();
+            
+            Console.WriteLine("Type y to battle.");
+            UserInput = Console.ReadLine();
+
+            while (UserInput == "y") {
+                Enemy Enemy = new Enemy();
+
+            }
         }
     }
 }
