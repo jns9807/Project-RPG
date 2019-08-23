0bd05d0e3a4b1abba321bec1165f9550b828bf3d
JoshuaKSmith <joshuaksmith17@gmail.com>
2019-08-22 23:18:56 -0500

    Added old code to character.cs

---
+++ Project RPG/Character/Character.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace Character //Changed this to be the package
+{
+    public abstract class Character
+    {
+        private int Level { get; }
+        private int CurrentExperience;
+        private int XpToLevel;
+        private int HitPoints;
+        private int MaxHitPoints;
+        private int Strength;
+        private int Intelligence;
+        private int Wisdom;
+        private int PhysDef;
+        private int MagDef;
+
+        public Character()
+        {
+            this.Level = 1;
+            this.CurrentExperience = 0;
+            this.XpToLevel = 100;
+            this.HitPoints = 100;
+            this.Strength = 10;
+            this.Intelligence = 10;
+            this.Wisdom = 10;
+            this.PhysDef = 10;
+            this.MagDef = 10;
+        }
+
+        public void PrintCharacter()
+        {
+            Console.WriteLine("Level: " + this.Level);
+            Console.WriteLine("Current Experience: " + this.CurrentExperience);
+            Console.WriteLine("Current HitPoints" + this.HitPoints);
+        }
+    }
+}
