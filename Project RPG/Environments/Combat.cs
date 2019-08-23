4cdd497df2947296b371a48c46deca9f30f5ca4e
JoshuaKSmith <joshuaksmith17@gmail.com>
2019-08-23 18:58:00 -0500

    Added Combat class into environment package.  Class will handle the combat environment obviously.

---
+++ Project RPG/Environments/Combat.cs
@@ -0,0 +1,14 @@
+using System;
+using Characters;
+using Enemies;
+
+namespace Environments {
+    
+    class Combat {
+        public static void Battle(Character Char1, Enemy Enemy1) {
+            while (Char1.HitPoints > 0) {
+
+            }
+        }
+    }
+}
